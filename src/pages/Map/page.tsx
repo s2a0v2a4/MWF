bc3252b1bf70678aaefa3bcb6f96c2209f5ab93e
s2a0v2a4 <xcrush389@gmail.com>
2025-07-16 11:37:02 +0200

    map design

--- src/pages/Map/page.tsx
+++ src/pages/Map/page.tsx
@@ -419,8 +419,8 @@ const MapPage = () => {
             dragging={true}
           >
             <TileLayer
-              attribution="© OpenStreetMap"
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution="Tiles © Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
+              url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
             />
             {/* User location marker */}
             {userLocation && (
